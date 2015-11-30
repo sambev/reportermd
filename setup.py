e1bf1595b8da0c9321dc8f85db2a782e54399460
Sam Beveridge <sbeveridge@saltstack.com>
2015-11-29 23:05:36 -0700

    Moved from scripts to console_scripts

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ from setuptools import setup
 setup(
     name='reportermd',
     packages=['reportermd'],
-    version='0.0.1',
+    version='0.0.4',
     description='Convert your Reporter App dates to MD then import to Day One',
     author='Sam Beveridge',
     author_email='sam.bev87@gmail.com',
     license='MIT',
     url='https://github.com/sambev/reportermd',
-    scripts=['bin/reportermd'],
+    entry_points={
+        'console_scripts': [
+            'reportermd=reportermd.cli:main',
+        ]
+    },
     include_package_data=True,
     install_requires=[
         'Jinja2>=2.8',
